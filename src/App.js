563ee64ec7642a6bb3cef9a43c95705f3d11fcce
[radhika] <[raoradhika2000@gmail.com]>
2023-09-11 17:36:51 +0530

    form design ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="box">
+        <div className="left-box">
+          <h3>Store data</h3>
+          <label htmlFor="name"> Name </label>
+          <input type="text" placeholder="Type your email" />
+          <label htmlFor="email"> Email </label>
+          <input type="email" placeholder="Type your email" />
+          <label htmlFor="number"> Number </label>
+          <input type="number" placeholder="Type your number" />
+          <button>Store my data</button>
+        </div>
+        <div className="right-box"></div>
+      </div>
     </div>
   );
 }
