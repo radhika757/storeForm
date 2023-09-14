7f2adc9e5633d27a9236250924e6cfcb1de9ccb2
[radhika] <[raoradhika2000@gmail.com]>
2023-09-14 17:34:33 +0530

    buttons added

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ function App() {
   const [email, setEmail] = useState("");
   const [number, setNumber] = useState("");
 
+  function handleSubmission(e) {
+    console.log(e.target.value);
+  }
   return (
     <div className="background">
       <div className="box">
         <div className="left-box">
           <h3>Store data</h3>
           <label htmlFor="name"> Name </label>
-          <input type="text" placeholder="Type your email" />
+          <input type="text" name="email" placeholder="Type your name" />
           <label htmlFor="email"> Email </label>
-          <input type="email" placeholder="Type your email" />
+          <input type="email" name="email" placeholder="Type your email" />
           <label htmlFor="number"> Number </label>
-          <input type="number" placeholder="Type your number" />
-          <button>Store my data</button>
+          <input type="number" name="num" placeholder="Type your number" />
+          <button onClick={handleSubmission}>Store my data</button>
         </div>
-        <div className="right-box"></div>
+        <div className="right-box">
+          <button>Get Data</button>
+          <button>Edit Data</button>
+        </div>
       </div>
     </div>
   );
